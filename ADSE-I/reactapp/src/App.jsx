30adaf9773fd40ab55ead0da5a206e271712b752
farazinam <farazinam17@gmail.com>
2026-01-03 17:02:48 +0500

    react - session 1

---
+++ ADSE-I/reactapp/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const name = "faraz";
+//   return (
+//     <>
+//     <h1>Hello World - My name is {name}</h1>
+//     <p>Learning React</p>
+//     </>
+//   )
+// }
+
+const App = () => {
+  const name = "Aptech";
+  return (
+    <>
+    <h1>Hello World - My name is {name}</h1>
+    <p>Learning React</p>
+    </>
+  )
+}
+
+export default App
