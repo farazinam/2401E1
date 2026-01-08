28a6231f9242774ca9e7eed19c90971dc07defbb
farazinam <farazinam17@gmail.com>
2026-01-08 17:27:27 +0500

    useState Hook & Event Handling

--- ADSE-I/reactapp/src/App.jsx
+++ ADSE-I/reactapp/src/App.jsx
@@ -47,39 +47,107 @@ import Card from './Components/card'
 
 
 // Children Props
-function Box({children}){
-  return <div style={{border: '2px solid gray', borderRadius: '10px'}}> {children} </div>
-}
+// function Box({children}){
+//   return <div style={{border: '2px solid gray', borderRadius: '10px'}}> {children} </div>
+// }
 
 
-// Nesting Props
-function Header(){
-  return <h1> NavBar </h1>
-}
+// // Nesting Props
+// function Header(){
+//   return <h1> NavBar </h1>
+// }
 
-function Footer(){
-  return <h1> All Right Reserved Aptech Laerning Malaysia - Copyright</h1>
-}
+// function Footer(){
+//   return <h1> All Right Reserved Aptech Laerning Malaysia - Copyright</h1>
+// }
 
-function Lab(){
+// function Lab(){
+//   return (
+//   <>
+//   {/* <Greet cn="Aptech" />
+//   <Age age={22} /> */}
+
+//   {/* <Box>
+//     <h1> React + Vite</h1>
+//     <p> Semester V</p>
+//   </Box> */}
+
+//   <Header />
+//   <div style={{display: 'flex'}}>
+//     <Card title="Wallpaper1" description="Computer Wallpaper" />
+//     <Card title="Wallpaper2" description="Computer Wallpaper" />
+//   </div>
+//   <Footer />
+//   </>
+//   )
+// }
+
+// export default Lab;
+
+
+// function App(){
+//   //const [count, setCount] = useState(0);   //useState
+
+//   function Clicked(){
+//     alert("Button Clicked!")
+//   }
+
+//   function Changed(e){
+//     console.log(e.target.value); 
+//   }
+
+//   return (
+//     <>
+//     {/* <h1>Counter</h1>
+//     <h4> Count : {count}</h4>
+//     <button onClick={()=> setCount(count + 1)}>Increment</button> */}
+
+
+//     <button onClick={Clicked}>Increment</button>
+//     <input type="text" onChange={Changed} />
+
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+
+
+// function LoginStatus({isLoggedin}){
+//   return (
+//     <h1> {isLoggedin ? "User Logged In" : "Plesae Log In"} </h1>
+//   )
+// }
+
+// function App(){
+//   return <LoginStatus isLoggedin={false} />
+// }
+
+// export default App;
+
+
+
+function App(){
+  const [isDark, setDark] = useState(false)
+
+  const toggleTheme=()=>{
+    setDark(!isDark);
+  }
+
+  const design = {
+    background: isDark ? "#000" : "#fff", 
+    color : isDark ? "#fff" : "#000"
+  }
+
   return (
-  <>
-  {/* <Greet cn="Aptech" />
-  <Age age={22} /> */}
-
-  {/* <Box>
-    <h1> React + Vite</h1>
-    <p> Semester V</p>
-  </Box> */}
-
-  <Header />
-  <div style={{display: 'flex'}}>
-    <Card title="Wallpaper1" description="Computer Wallpaper" />
-    <Card title="Wallpaper2" description="Computer Wallpaper" />
-  </div>
-  <Footer />
-  </>
+    
+    <body style={design}>
+    <button onClick={toggleTheme}> CHANGE THEME </button>
+    </body>
+    
   )
 }
 
-export default Lab;
+export default App;
