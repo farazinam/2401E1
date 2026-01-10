1dfdc4c3d8dd2af44ca10b951681428b2fc3a0a7
farazinam <farazinam17@gmail.com>
2026-01-10 17:17:59 +0500

    useEffect Hook & API Integration

--- ADSE-I/reactapp/src/App.jsx
+++ ADSE-I/reactapp/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -128,25 +128,151 @@ import Card from './Components/card'
 // export default App;
 
 
+// function App(){
+//   const [isDark, setDark] = useState(false)
 
-function App(){
-  const [isDark, setDark] = useState(false)
+//   const toggleTheme=()=>{
+//     setDark(!isDark);
+//   }
 
-  const toggleTheme=()=>{
-    setDark(!isDark);
-  }
+//   const design = {
+//     background: isDark ? "#000" : "#fff", 
+//     color : isDark ? "#fff" : "#000"
+//   }
 
-  const design = {
-    background: isDark ? "#000" : "#fff", 
-    color : isDark ? "#fff" : "#000"
-  }
+//   return (
+    
+//     <body style={design}>
+//     <button onClick={toggleTheme}> CHANGE THEME </button>
+//     </body>
+    
+//   )
+// }
+
+// export default App;
+
+
+
+//-------------- Day 4 ----------------
+
+// function LoginForm(){
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const [showPass, setShowPass] = useState(false);
+
+//   function getData(e){
+//     e.preventDefault();
+//     console.log(email);
+//     console.log(password);  
+//   }
+
+//   return (
+//     <form onSubmit={getData}>
+//     <input 
+//     type="text"
+//     placeholder='Enter Your Email'
+//     value={email}
+//     onChange={(e)=> setEmail(e.target.value)} /> <br />
+
+//     <input 
+//     type={showPass ? "text" : "password"}
+//     placeholder='Enter Your Password'
+//     value={password}
+//     onChange={(e)=> setPassword(e.target.value)} />
+
+//     <span onClick={(e) => setShowPass(!showPass)}> { showPass ? "🙈" : "👁" } </span> <br />
+
+//     <button type="submit">Get</button>
+//     </form>
+//   )
+
+// }
+
+// export default LoginForm;
+
+
+//---use effect ----
+
+function App1(){
+  useEffect(()=>{
+    console.log("This is Use Effect with no dependency Array");
+  })
 
   return (
-    
-    <body style={design}>
-    <button onClick={toggleTheme}> CHANGE THEME </button>
-    </body>
-    
+    <h1>Use Effect - no dependency Array</h1>
+  )
+}
+
+function App2(){
+  useEffect(()=>{
+    console.log("This is Use Effect with empty dependency Array");
+  },[])
+
+  return (
+    <h1>Use Effect - empty dependency Array</h1>
+  )
+}
+
+
+function App3(){
+  const [count, setCount] = useState(0);   //useState
+
+  useEffect(()=>{
+    console.log("Count Updated: " + count);
+  },[count])
+
+  return (
+    <>
+    <h1>Counter</h1>
+    <h4> Count : {count}</h4>
+    <button onClick={()=> setCount(count + 1)}>Increment</button>
+
+    </>
+  )
+}
+
+// ----- Fetch data - API ----
+function FetchData(){
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=> res.json())
+    .then((data) =>{
+      setUser(data),
+      setLoading(false);
+    }
+  )
+  },[])
+
+  return (
+    <>
+    <h1> API DATA</h1>
+    {loading && <p>Loading...</p>}
+
+    <ul>
+      {
+        user.map((user)=>(
+          <li key={user.id}>
+           <span>Name:</span> {user.name} <br /> <span>Email:</span> {user.email}
+          </li>
+        ))
+      }
+    </ul>
+    </>
+  )
+}
+
+function App(){
+  return (
+    <>
+    {/* <App1 /> */}
+    {/* <App2 /> */}
+    {/* <App3 /> */}
+    <FetchData />
+    </>
   )
 }
 
